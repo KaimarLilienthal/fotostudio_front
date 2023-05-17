21a151cf2d666c2c7d7334e3941d091b38ab76ea
Kaimar <kaimarlilienthal@gmail.com>
2023-05-17 16:01:33 +0300

    Initial commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+      <button @click="login" type="submit">Test123</button>
   </div>
+    <button type="button" class="btn btn-primary">Primary</button>
+
 </template>
+
+<script>
+
+export default {
+    name: 'AboutView',
+    methods: {
+        login: function () {
+            this.$http.get("/login", {
+                    params: {
+                        username: 'kaimar',
+                        password: '123'
+                    }
+                }
+            ).then(response => {
+                const responseBody = response.data
+            }).catch(error => {
+                const errorResponseBody = error.response.data
+            })
+        },
+    }
+}
+
+</script>
+
