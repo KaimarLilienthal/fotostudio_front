325b9dd676267219a2f4847dc417c9cef4faf84b
Kaimar <kaimarlilienthal@gmail.com>
2023-05-29 16:09:55 +0300

    Kaimar Wip E

--- src/views/StudioGeneralAvailabilityView.vue
+++ src/views/StudioGeneralAvailabilityView.vue
@@ -23,7 +23,8 @@
             <div class="col col-3">
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">Algus</span>
-                    <input v-model="startDate" type="date" class="form-control" aria-describedby="basic-addon1">
+                    <input v-model="startDate" type="date" class="form-control"
+                           aria-describedby="basic-addon1">
                 </div>
             </div>
         </div>
@@ -31,7 +32,8 @@
             <div class="col col-3">
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">Lõpp</span>
-                    <input v-model="endDate" type="date" class="form-control" aria-describedby="basic-addon1">
+                    <input v-model="endDate" type="date" class="form-control"
+                           aria-describedby="basic-addon1">
                 </div>
             </div>
         </div>
@@ -44,7 +46,8 @@
             <div id="app" class="input-group mb-3">
 
                 <span class="input-group-text" id="basic-addon1">Algus</span>
-                <input v-model="startHour" type="time" id="start" class="form-control" aria-describedby="basic-addon1" >
+                <input v-model="startHour" type="time" id="start" class="form-control"
+                       aria-describedby="basic-addon1">
             </div>
         </div>
     </div>
@@ -52,7 +55,8 @@
         <div class="col col-2">
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Lõpp</span>
-                <input v-model="endHour" type="time" id="stop" class="form-control" aria-describedby="basic-addon1" >
+                <input v-model="endHour" type="time" id="stop" class="form-control"
+                       aria-describedby="basic-addon1">
 
             </div>
             <div class="col mb-12">
@@ -62,47 +66,69 @@
 
         </div>
     </div>
-    <div class="container">
-        <div class="row">
+    <div class="row">
+        <div class="col">
             <table class=" table table-dark table-hover">
                 <thead>
                 <tr>
-                    <div class="row mb-5 justify-content-center ">
-                        <div class="col col-4">
+
                             <th scope="col">Kuupäev alates</th>
                             <th scope="col">Kuupäev kuni</th>
-                            <th scope="col">Kellaaeg</th>
+                            <th scope="col">Kellaaeg alates</th>
+                            <th scope="col">Kellaaeg kuni</th>
                             <th scope="col">kustuta</th>
 
-                        </div>
-                    </div>
                 </tr>
                 </thead>
+                <tbody>
+                <tr v-for="availability in availabilities" :key="studioId">
+                    <th>{{ availability.startDate }}</th>
+                    <td>{{ availability.endDate }}</td>
+                    <td>{{ availability.startHour }}</td>
+                    <td>{{ availability.endHour }}</td>
+                    <td>
+                        <font-awesome-icon  class="hoverable-link" :icon="['fas', 'trash']" />
+                    </td>
+                </tr>
+                </tbody>
             </table>
         </div>
     </div>
-
+    <SuccessModal :message="successMessage" ref="successModalRef" @event-success="handleSuccessMessage"/>
+<!--      <DangerModal :message={"errorResponse.message" ref="dangerModalRef" @event-danger="handleDangerMessage"/>-->
 </template>
 
 <script>
 import {useRoute} from "vue-router";
 
 import router from "@/router";
+import DangerModal from "@/components/modal/alertmodals/DangerModal.vue";
+import SuccessModal from "@/components/modal/alertmodals/SuccessModal.vue";
+import StudioImage from "@/components/StudioImage.vue";
 
 
 export default {
 
     name: "StudioGeneralAvailabilityView",
-    components: {},
+    components: {StudioImage, SuccessModal, DangerModal},
 
     data() {
         return {
-            startDate: 0,
-            endDate: 0,
-            startHour: 0,
-            endHour: 0,
+            startDate: "",
+            endDate: "",
+            startHour: "",
+            endHour: "",
+            successMessage: '',
             studioId: Number(useRoute().query.studioId),
             studioName: String(useRoute().query.studioName),
+            availabilities: {
+
+                startDate: "",
+                endDate: "",
+                startHour: "",
+                endHour: ""
+
+            }
         }
     },
     watch: {
@@ -129,18 +155,56 @@ export default {
             router.push({name: 'reservationRoute', query: {studioId: this.studioId, studioName: this.studioName}})
         },
         show() {
-            alert(this.startDate)
-            alert(this.endDate)
-            alert(this.startHour)
-            alert(this.endHour)
+            if (this.startDate == '' || this.endDate == '' || this.startHour == '' || this.endHour == '') {
+                this.successMessage = 'Palun täida kõik väljad!'
+                this.$refs.successModalRef.$refs.modalTemplateRef.openModal()
+            } else {
+                this.availabilities.startDate = this.startDate
+                this.availabilities.endDate = this.endDate
+                this.availabilities.startHour = this.startHour
+                this.availabilities.endHour = this.endHour
+                this.$http.post("/booking/availability", this.availabilities, {
+                        params: {
+                            studioId: this.studioId
+                        }
+                    }
+                ).then(response => {
+                    this.successMessage = 'Lahtioleku aeg lisatud!'
+                    this.$refs.successModalRef.$refs.modalTemplateRef.openModal()
+                }).catch(error => {
+                    router.push({name: 'errorRoute'})
+                })
+
+            }
+
+        },
+        getStudioAvailabilities: function () {
+            this.$http.get("/booking/availability", {
+                    params: {
+                        studioId: this.studioId,
+                    }
+                }
+            ).then(response => {
+                this.availabilities = response.data
+            }).catch(error => {
+                router.push({name: 'errorRoute'})
+            })
+        },
+
+
+        handleSuccessMessage() {
+        },
+        handleDangerMessage(){
 
         }
 
+    },
+    beforeMount() {
+        this.getStudioAvailabilities();
     }
 
 
 }
-
 
 
 </script>
